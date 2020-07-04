c9cf5b1f221aaf1c4db02ebfff5ad436e5b9bee2
Dave W Wright <dave.william.wright@gmail.com>
2020-07-04 19:57:19 +0100

    Correct env variable to SITENAME with no _

--- superlists/settings.py
+++ superlists/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if 'DJANGO_DEBUG_FALSE' in os.environ:
     DEBUG = False
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-    ALLOWED_HOSTS = [os.environ['SITE_NAME']]
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
 else:
     DEBUG = True
     # Insecure key for dev
