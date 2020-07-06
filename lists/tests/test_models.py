de6ff87b66e8c824303b190dd87792417b7c7d06
Dave W Wright <dave.william.wright@gmail.com>
2020-07-06 18:25:23 +0100

    Split out unit tests into two files

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,31 +1,5 @@
 from django.test import TestCase
-from django.template.loader import render_to_string
 from lists.models import Item, List
-
-from lists.views import home_page
-
-
-class HomePageTest(TestCase):
-
-    def test_uses_home_template(self):
-
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
-
-
-class NewListTest(TestCase):
-
-    def test_can_save_a_POST_request(self):
-        self.client.post('/lists/new', data={'item_text': 'A new list item'})
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_redirects_after_POST(self):
-        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
-        new_list = List.objects.first()
-        self.assertRedirects(response, f'/lists/{new_list.id}/')
 
 
 class ListAndItemModelsTest(TestCase):
