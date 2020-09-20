a9d013a17f58a2d06f25b2c5293e2f9c4bbb548d
Dave W Wright <dave.william.wright@gmail.com>
2020-09-20 14:17:15 +0100

    Use ExistingListItemForm in the list view + fix functional test for the error message

--- lists/forms.py
+++ lists/forms.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ValidationError
 from lists.models import Item
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
-DUPLICATE_ITEM_ERROR = "You've already got this in yout list"
+DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
 class ItemForm(forms.models.ModelForm):
 
@@ -33,11 +33,13 @@ class ExistingListItemForm(ItemForm):
         super().__init__(*args, **kwargs)
         self.instance.list = for_list
 
-
     def validate_unique(self):
         try:
             self.instance.validate_unique()
         except ValidationError as e:
             e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
             self._update_errors(e)
+
+    def save(self):
+        return forms.models.ModelForm.save(self)
 
